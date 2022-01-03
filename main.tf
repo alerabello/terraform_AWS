012c442ff350d1bc3e0dd2610c8b201723eaa0f3
Alexandre Rabello <42966276+alerabello@users.noreply.github.com>
2022-01-03 15:46:01 -0300

    main

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ variable "do_token" {
 
 resource "aws_droplet" "web" {
   image = "ami-0b0ea68c435eb488d (64-bit x86)"
-  name = "web-1"
+  name = "web"
   region = "us-east-1"
   size = "t2.micro"
 }
