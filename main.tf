6bcbfbea144630ac833dad5b393d4346140872b0
Alexandre Rabello <42966276+alerabello@users.noreply.github.com>
2022-01-03 15:39:11 -0300

    main

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    token = var.do_token
+}
+
+variable "do_token" {  
+}
+
+resource "aws_droplet" "web" {
+  image = "ami-0b0ea68c435eb488d (64-bit x86)"
+  name = "web-1"
+  region = "us-east-1"
+  size = "t2.micro"
+}
