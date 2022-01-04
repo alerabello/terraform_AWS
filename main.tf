d89fb0c9e1ca2ce0b66084f9a39c083cb28293f3
Alexandre Rabello <42966276+alerabello@users.noreply.github.com>
2022-01-04 13:38:20 -0300

    Att 0.0.2 Beta

--- main.tf
+++ main.tf
@@ -1,13 +1,17 @@
+# Alexandre R Santana
+# Documentação: https://registry.terraform.io/providers/hashicorp/aws/latest/docs
+# Videos Aula: https://www.udemy.com/course/aws-com-terraform
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configurando AWS Cloud 
 provider "aws" {
-    token = var.do_token
-}
-
-variable "do_token" {  
-}
-
-resource "aws_droplet" "web" {
-  image = "ami-0b0ea68c435eb488d (64-bit x86)"
-  name = "web"
   region = "us-east-1"
-  size = "t2.micro"
 }
